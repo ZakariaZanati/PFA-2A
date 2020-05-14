63680b6516ea6d2d9b981ff5439cd3491aaf03af
younessmouad <ymouad96@gmail.com>
2020-05-14 18:36:26 +0000

    Update relationsController.js
    Ajout des fonctionnalités suivantes:
    -> possibilité d'envoi d'une demande de fin de contrat.
    -> les types demandes sont affichés.
    ->  fonctionnalité de mettre fin au contrat apres acceptation du medecin.
    -> fonctionnalités d'affichage dans le profile d'un medecin selon sa situation actuelle avec l'utilisateur.

--- controllers/relationsController.js
+++ controllers/relationsController.js
@@ -8,6 +8,14 @@ module.exports = function (app , mongoose) {
         extended: false
     })
 
+    function getFormatDate(){
+        var dateArray = Date().split(' ');
+        var _month = new Date().getMonth() + 1;
+        var month = _month < 10 ? '0' + _month : _month;
+        var date = dateArray[3] + '-' + month + '-' + dateArray[2];
+        var time = dateArray[4];  
+        return [date, time];
+    }
 
     app.get('/medecins',(req,res)=>{
         if (req.session.type === 'normal') {
@@ -35,7 +43,30 @@ module.exports = function (app , mongoose) {
             id = req.query.id;
             Medecin.findById(id)
             .then((medecin)=>{
-                res.render('medecinProfile',{medecin : medecin});
+                User.findById(req.session.userId)
+                .then((user) => {
+                    var isMedecin = user.medecins.find(_medecin => _medecin.medecin == id && _medecin.finContrat == null);
+                    var haveMedecin = user.medecins.some((medecin)=>{
+                        return medecin.finContrat == null;
+                    });
+                    var sentDemande = medecin.demandes.find(demande => demande == user.id);
+                    if(haveMedecin) {
+                        if(isMedecin) {
+                            var debutContrat = isMedecin.debutContrat;
+                            sentDemande ? 
+                                res.render('medecinProfile',{medecin : medecin, estMedecin: true, sentDemande: true, debutContrat: debutContrat})
+                                : res.render('medecinProfile',{medecin : medecin, estMedecin: true, sentDemande: false, debutContrat: debutContrat});
+                        }
+                        else {
+                            res.render('medecinProfile',{medecin : medecin, estMedecin: false})
+                        }
+                        
+                    }
+                    else {
+                        sentDemande ? res.render('medecinProfile',{medecin : medecin, estMedecin: null, sentDemande: true})
+                                    : res.render('medecinProfile',{medecin : medecin, estMedecin: null, sentDemande: false});
+                    }
+                })
             })
         }
         else if(req.session.type === 'medecin') {
@@ -55,39 +86,64 @@ module.exports = function (app , mongoose) {
 
     app.post('/medecinProfile',urlencodedParser,(req,res,next)=>{
         id = req.query.id;
+        action = req.query.action;
         if (req.session.type === 'normal') {
-            User.findById(req.session.userId)
-            .then((user)=>{
-                if (user.medecin != null) {
-                    var err = new Error("Vous disposez déjà d'un medecin ");
-                    next(err);
-                }
-                else{
-                    Medecin.findById(id)
-                    .then((medecin)=>{
-                        var isInArray = medecin.demandes.some((user)=>{
-                            return user.equals(req.session.userId);
-                        })
-                        if (!isInArray) {
-                            medecin.demandes.push(req.session.userId);
-                            medecin.save();
-                        }
+            if(action === 'yes') {
+                User.findById(req.session.userId)
+                .then((user)=>{
+                    var haveMedecin = user.medecins.find(medecin => medecin.finContrat == null);
+                    if (haveMedecin) {
+                        var err = new Error("Vous disposez déjà d'un medecin ");
+                        next(err);
+                    }
+                    else{
+                        Medecin.findById(id)
+                        .then((medecin)=>{
+                            var isInArray = medecin.demandes.some((user)=>{
+                                return user.equals(req.session.userId);
+                            })
+                            if (!isInArray) {
+                                medecin.demandes.push(req.session.userId);
+                                medecin.save();
+                            }
+    
+                        })                    
+                    }
+                })
+                .catch((err)=>next(err));
+            }
+            else if(action === 'end') {
+                Medecin.findById(id)
+                .then((medecin)=>{
+                    var isInArray = medecin.demandes.some((user)=>{
+                        return user.equals(req.session.userId);
+                    })
+                    if (!isInArray) {
+                        medecin.demandes.push(req.session.userId);
+                        medecin.save();
+                    }
 
-                    })                    
-                }
-            })
-            .catch((err)=>next(err));
+                }) 
+            }
         }
         res.redirect('medecins');
     })
-
 
     app.get('/demandes',(req,res)=>{
         if (req.session.type === 'medecin') {
             Medecin.findById(req.session.userId)
             .populate('demandes')
             .then((medecin)=>{
-                res.render('demandes',{demandes : medecin.demandes});
+                var allDemandes = [];
+                medecin.demandes.forEach(demande => {
+                    var estMedecin = medecin.utilisateurs.find(user => user.utilisateur == demande.id && user.finContrat == null);
+                    estMedecin ?
+                        allDemandes.unshift({user: demande, type: "Fin contrat"}) 
+                            : allDemandes.unshift({user: demande, type: "Debut contrat"}) 
+                    
+                })
+                console.log(allDemandes);
+                res.render('demandes',{demandes : allDemandes});
             })
         }
         else if(req.session.type === 'normal') {
@@ -112,14 +168,19 @@ module.exports = function (app , mongoose) {
             .then((user)=>{
                 Medecin.findById(req.session.userId)
                 .then((medecin) => {
+                    console.log("HHHHHHHHHHHH");
                     var isInArray = medecin.utilisateurs.some((user)=>{
-                        return user.equals(id);
+                        return user.utilisateur.equals(id) && user.finContrat == null;
                     })
+                    console.log("==============  " + isInArray);
                     if(isInArray) {
-                        res.render('userProfile',{user : user, estPatient: "yes"});
+                        var demandeFin = medecin.demandes.find(demande => demande == id);
+                        console.log(demandeFin);
+                        demandeFin ? res.render('userProfile',{user : user, estPatient: true, demandeFin: true})
+                                   : res.render('userProfile',{user : user, estPatient: true, demandeFin: false});
                     }
                     else {
-                        res.render('userProfile',{user : user, estPatient: "no"});
+                        res.render('userProfile',{user : user, estPatient: false});
                     }
                 });
             })
@@ -142,16 +203,15 @@ module.exports = function (app , mongoose) {
             if (action === 'yes') {
                 Medecin.findById(req.session.userId)
                 .then((medecin)=>{
-                    medecin.utilisateurs.push(patientId);
+                    medecin.utilisateurs.push({utilisateur: patientId});
                     medecin.demandes.pull(patientId);
                     medecin.save();
                     User.findById(patientId)
                     .then((user)=>{
                         console.log("HI HERE");
                         console.log(user.nom);
-                        user.medecin = req.session.userId;
+                        user.medecins.push({medecin: req.session.userId});
                         user.save();
-                        console.log("User medecin:" + user.medecin);
                         Medecin.find({})
                         .then((medecins)=>{
                             medecins.forEach((medecin)=>{
@@ -174,16 +234,44 @@ module.exports = function (app , mongoose) {
                     medecin.save();
                 })
             }
-            else if(action === 'end') {
+            else if(action === 'end') {   
+                var currentDate = getFormatDate();
+                Medecin.findByIdAndUpdate(req.session.userId, 
+                    {$set: {"utilisateurs.$[user].finContrat": new Date()}},
+                    {arrayFilters: [{"user.utilisateur": patientId, "user.finContrat": null}]})
+                    .then((medecin) => {
+                        User.findByIdAndUpdate(patientId, 
+                            {$set: {"medecins.$[medec].finContrat": new Date()}},
+                            {arrayFilters: [{"medec.medecin": req.session.userId, "medec.finContrat": null}]})
+                            .then((doc) => {
+                                console.log(doc);
+                            })
+                    })  
+            }
+            else if(action === 'yesEnd') {
                 Medecin.findById(req.session.userId)
                 .then((medecin)=>{
-                    medecin.utilisateurs.pull(patientId);
+                    medecin.demandes.pull(patientId)
                     medecin.save();
-                    User.findById(patientId)
-                    .then((user)=>{
-                        user.medecin = undefined;
-                        user.save();
-                    });
+                });
+                var currentDate = getFormatDate();
+                Medecin.findByIdAndUpdate(req.session.userId, 
+                    {$set: {"utilisateurs.$[user].finContrat": new Date()}},
+                    {arrayFilters: [{"user.utilisateur": patientId, "user.finContrat": null}]})
+                    .then((medecin) => {
+                        User.findByIdAndUpdate(patientId, 
+                            {$set: {"medecins.$[medec].finContrat": new Date()}},
+                            {arrayFilters: [{"medec.medecin": req.session.userId, "medec.finContrat": null}]})
+                            .then((doc) => {
+                                console.log(doc);
+                            })
+                    }) 
+            }
+            else if(action === 'noEnd') {
+                Medecin.findById(req.session.userId)
+                .then((medecin)=>{
+                    medecin.demandes.pull(patientId)
+                    medecin.save();
                 })
             }
             res.redirect('demandes')
@@ -201,9 +289,20 @@ module.exports = function (app , mongoose) {
         }
         else if(req.session.type === 'medecin') {
             Medecin.findById(req.session.userId)
-            .populate('utilisateurs')
+            .populate('utilisateurs.utilisateur')
             .then((medecin) => {
-                res.render('patients', {patients: medecin.utilisateurs});
+                var oldPatients = [];
+                var currentPatients = [];
+                medecin.utilisateurs.forEach((user) =>
+                {
+                    if(user.finContrat == null) {
+                        currentPatients.push(user);
+                    }
+                    else {
+                        oldPatients.push(user);
+                    }
+                })
+                res.render('patients', {allPatients: medecin.utilisateurs, oldPatients: oldPatients, currentPatients: currentPatients});
             })
         }
         else {
