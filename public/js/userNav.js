f01861e5c44e8bf54bff1f521a63a09082fdad53
Errahmi Ayoub <ayoub_errahmi@um5.ac.ma>
2020-05-25 21:09:41 +0000

    smartWatch

--- public/js/userNav.js
+++ public/js/userNav.js
@@ -1,14 +1,21 @@
 var patientNav = document.getElementById("navPatient");
 var medecinNav = document.getElementById("navMedecin");
+var patientH1 = document.getElementById("h1Patient");
+var medecinH1 = document.getElementById("h1Medecin");
 var url_string = window.location.href;
 var url = new URL(url_string);
 var param = url.searchParams.get("id");
 
-if(param === null){
+if (param === null) {
     medecinNav.style.display = "none";
     patientNav.style.display = "block";
+    medecinH1.style.display = "none";
+    patientH1.style.display = "block";
+
 }
 else {
     medecinNav.style.display = "block";
     patientNav.style.display = "none";
+    medecinH1.style.display = "block";
+    patientNavH1.style.display = "none";
 }
